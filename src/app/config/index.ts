4eba7d4d37ceae7b7cdd7e2545f1350d442b1e5e
leon-dream1~ <nahidulislam019045@gmail.com>
2024-11-27 18:18:38 +0600

    update port

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,6 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-  port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
 };
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
 
+const port = process.env.PORT || 5000;
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
     });
   } catch (error) {
     console.log(error);
